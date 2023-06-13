17957e0d028a3ece3df885176b876593d4fdfd85
hannah brown <hannah.brown@ons.gov.uk>
2023-06-13 11:01:28 +0000

    Updated descriptions
    descriptions updated within table_utils:
    read 3 main tables

--- src/table_utils.py
+++ src/table_utils.py
@@ -15,30 +15,39 @@ def read_spend_merchant_location(
     cardholder_issuing_country="",
 ):
     """
-    Description:
-    Function allows you to read in data from the spend merchant location table\
-    with default values set to 'All' where possible.
-    so if you wanted to keep the defaults you would run:\
-    read_spend_merchant_location(client). However, if you wanted to change
-    one or more of the categorical variables you would run\
-    read_spend_merchant_location(client, merchant_location_level = 'POSTAL_SECTOR').\
-    If you want a value to cover anything then define it as an empty string e.g.\
-    mcg = ''
+\n
+\n
+Description:
+Function allows you to read in data from \
+spend_merchant_location with \
+default values set to 'All' where possible. \n
+To keep the default arguments run:
+read_spend_merchant_location(client) \n
+To filter on one or more variables, \
+change the arguments within the function
+For example:
+read_spend_merchant_location\
+(client, merchant_location_level = 'POSTAL_SECTOR') \n
+To include every option within a variable \
+define it as an empty string
+For example:
+read_spend_merchant_location(client, mcg = '')
 
     Args:
     - client: defined earlier in the session
-    - time_period: 'Month' or 'Quarter'. Defaults to 'All'
+    - time_period: 'Month' or 'Quarter'. Defaults to 'Quarter'
     - merchant_location_level: string. Defaults to 'All'
+    - merchant_location: string. Defaults to ''
+        (All merchant locations included)
     - cardholder_issuing_level: string. Defaults to 'All'
+    - cardholder_issuing_country: string. Defaults to ''
+        (All cardholder issuing countries are included)
     - mcg: string. Defaults to 'All'
     - mcc: string. Defaults to 'All'
-    - merchant_location: string. defaults to '' so that all
-    merchant locations are picked up
-    - cardholder_issuing_country: string. defaults to '' so that all cardholder
-    issuing countries are picked up.
 
-    Returns:
-       Spend Merchant Location with specification of your choice
+
+Returns:
+   Spend Merchant Location with specification of your choice
     """
 
     if merchant_location != "":
@@ -97,37 +106,48 @@ def read_spend_origin_and_channel(
     destination_country="",
 ):
     """
-    Description:
-    Function allows you to read in data from the spend_origin_and_channel\
-    table with default values set to 'All' where possible.
-    so if you wanted to keep the defaults you would run:\
-    read_spend_origin_and_channel(client). However, if you wanted to change
-    one or more of the categorical variables you would run\
-    read_spend_origin_and_channel(client, merchant_channel = 'Face to Face').\
-    If you want a value to cover anything then define it as an empty string e.g.\
-    mcg = ''
+\n
+\n
+Description:
+Function allows you to read in \
+data from spend_origin_and_channel \
+table with default values set to 'All' where possible. \n
+To keep the default arguments run:
+read_spend_origin_and_channel(client) \n
+To filter on one or more variables, \
+change the arguments within the function
+For example:
+read_spend_origin_and_channel\
+(client, merchant_channel = 'Face to Face') \n
+To include every option within a variable \
+define it as an empty string
+For example:
+read_spend_origin_and_channel(client,mcg = '')
+
 
     Args:
     - client: defined earlier in the session
-    - time_period: 'Month' or 'Quarter'. Defaults to 'All'
+    - time_period: 'Month' or 'Quarter'. Defaults to 'Quarter'
     - cardholder_origin: string. Defaults to 'All'
-    - cardholder_origin_country: string. Defaults to 'All'. If not 'All', it changes
-    cardholder origin to be "".
+    - cardholder_origin_country: string. Defaults to 'All'.
     - mcg: string. Defaults to 'All'
-    - mcc: boolean. Defaults to True. If False it will gather all mccs
-    under the MCG specified.
+    - mcc: string. Defaults to 'All'
     - merchant_channel: string. Defaults to 'All'
-    - cardholder_location: string. defaults to '', meaning it can take any value.
-    To pick a specific cardholder location(s)
-    run it in the following format: cardholder_location = " 'SW2' " or
-    cardholder_location = " 'SW2', 'SE4' " for multiple
-    - destination_country: string. defaults to '', meaning it can take any value.
-    to pick a specific destination country(s)
-    run it in the following format: destination_country = " 'UNITED KINGDOM' " or
-    destination_country = " 'UNITED KINGDOM', 'FRANCE' " for multiple
+    - cardholder_location: string. Defaults to ''
+    (To filter on specific cardholder location(s) \
+    run it in the following format:
+    cardholder_location = " 'SW2' " or\
+    cardholder_location = " 'SW2', 'SE4' " for multiple)
+    - destination_country: string. Defaults to ''
+    (All destination countries are given.
+    To choose specific countries use the following format:
+    destination_country = " 'UNITED KINGDOM' " or
+    destination_country = " 'UNITED KINGDOM'\
+    , 'FRANCE' " for multiple)
 
-    Returns:
-    Spend Merchant Location with specification of your choice
+
+Returns:
+Spend Merchant Location with specification of your choice
     """
 
     if cardholder_location != "":
@@ -199,34 +219,53 @@ def read_retail_performance_high_streets_towns(
     merchant_location="",
 ):
     """
-    Description:
-    Function allows you to read in data from the spend merchant location\
-    table with default values set to 'All' where possible.
-    so if you wanted to keep the defaults you would run: \
-    read_retail_performance_high_streets_towns(client). However,\
-    if you wanted to change
-    one or more of the categorical variables you would run\
-    read_retail_performance_high_streets_towns(client,\
-    merchant_location_level = 'POSTAL_SECTOR').
+\n
+\n
+Description:
+Function allows you to read in data from \
+read_retail_performance_high_streets_towns \
+table with default values set to 'All' where possible.\n
+To keep the default arguments run:
+read_retail_performance_high_streets_towns(client)\n
+To filter on one or more variables, \
+change the arguments within the function
+For example:
+read_retail_performance_high_streets_towns(client, \
+merchant_location_level = 'POSTAL_SECTOR')\n
+To include every option within a variable \
+define it as an empty string
+For example:
+read_retail_performance_high_streets_towns\
+(client, mcg = '')
+
 
     Args:
     - client: defined earlier in the session
-    - time_period: 'Month' or 'Quarter'. Defaults to 'All'. Choose "" for any.
-    - merchant_location_level: string. Defaults to 'All'. Choose "" for any.
-    - cardholder_issuing_level: string. Defaults to 'All'. Choose "" for any.
-    - mcg: string. Defaults to 'All'. Choose "" for any. Choose "" for any.
-    - cardholder_location: string. defaults to "" but to pick a
-    specific cardholder location(s)
-    run it in the following format: cardholder_location = " 'SW2' "
-    or cardholder_location =" 'SW2', 'SE4' " for multiple
-    - merchant_location: string. defaults to "" but to pick a specific
-    merchant location(s)
-    run it in the following format: merchant_location = " 'SW2' " or
-    merchant_location = " 'SW2', 'SE4' " for multiple
+    - time_period: 'Month' or 'Quarter'. Defaults to 'Quarter'.
+    - merchant_location_level: string. Defaults to 'All'
+    - merchant_location: string. Defaults to ''
+        (To choose specific merchant location(s)\
+        use the following format:
+         merchant_location = " 'SW2' " or\
+         merchant_location =" 'SW2', 'SE4' " for multiple)
+    - cardholder_issuing_level: string. Defaults to 'All'
+    - cardholder_location: string. Defaults to ''
+         (To choose specific cardholder location(s) \
+         use the following format:
+         cardholder_location = " 'SW2' " or\
+         cardholder_location =" 'SW2', 'SE4' " for multiple)
+    - mcg: string. Defaults to 'All'.
+    - mcc: string. Defaults to 'All'.
 
-    Returns:
-    retail_performance_high_streets_towns with specification of your choice
-    """
+    If a specific cardholder_location or\
+    merchant_location is defined,
+    location_level filters automatically\
+    to accomodate the specified location
+
+
+Returns:
+retail_performance_high_streets_towns with specification of your choice
+"""
 
     # If we choose specific locations, we need to allow location levels
     # to be anything rather than fixed
